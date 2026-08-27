f9f2b5bdb899b5a4b05c490b6c11251e003716f4
mohmand ibrahim <mi1482@fayoum.edu.eg>
2026-08-27 04:55:26 +0300

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Timers;
+using static Program;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
